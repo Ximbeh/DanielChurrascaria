02e1b1a21758020b1c9f7076350c68df9a725b8a
Ximbeh <senhorximbinha@gmail.com>
2024-02-03 14:37:00 -0300

    .

---
+++ Ação 3/script.js
@@ -0,0 +1,8 @@
+function abrirJanela(){
+    document.getElementById('janela').style.width = "250px";
+
+} 
+
+function fecharJanela(){
+    document.getElementById('janela').style.width = "0px";
+}
